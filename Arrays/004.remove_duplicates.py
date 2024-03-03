1d6cdd4389b27fc32ea596fa8b55212a3620f037
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-03 15:32:54 +0530

    Remove duplicates

--- Arrays/004.remove_duplicates.py
+++ Arrays/004.remove_duplicates.py
@@ -1,2 +1,23 @@
 # Remove duplicates from sorted array
 # set not allow duplicate values
+from  array import array
+
+def remove_duplicates(nums):
+    if not nums:
+        return 0
+    unique_index =0
+    
+    for i in range (1,len(nums)):
+        if nums[i] != nums[unique_index]:
+            unique_index+=1
+            nums[unique_index] = nums[i]
+    return unique_index+1
+sortedArray = [1,1,1,2,2,3,3,4,4] 
+s = remove_duplicates(sortedArray)
+print(sortedArray[:s])
+
+""" 
+OUTPUT:
+[1, 2, 3, 4]
+ """
+
