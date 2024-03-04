5a6554a2ccf7dae6b692be86e4a8f2e9f7c6bf6f
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-04 12:17:25 +0530

    Left rotate 1 place

--- Arrays/05.left_rotate.py
+++ Arrays/05.left_rotate.py
@@ -1,1 +1,15 @@
        # Left Rotate the Array by One Place
+from array import array
+
+def left_rotate(arr):
+    temp = arr[0]
+    for i in range(len(arr)-1):
+        arr[i] = arr[i+1]
+    arr[-1] = temp
+   
+    
+
+arr= [1,2,3,4,5]
+left_rotate(arr)
+print(arr)
+
