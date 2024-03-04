d0a4a24ec01424940aaf72fa4fab9a5bda2ad19d
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-04 14:46:11 +0530

    Brute Code

---
+++ Arrays/06.left_rotate_dplaces.py
@@ -0,0 +1,16 @@
+from array import array
+
+def left_rotate(arr,k):
+    n =len(arr)
+    k = k % n
+    for _ in range(k):
+        temp = arr[0]
+        for i in range(1,n):
+            arr[i-1] = arr[i]
+        arr[n-1] = temp
+
+arr = [1,2,4,5,6]
+k = 2
+left_rotate(arr,k)
+print(arr)
+    
