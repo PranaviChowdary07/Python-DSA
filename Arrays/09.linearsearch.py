9395767c9a7df57f58012be8336d28bba7feb109
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-05 17:08:11 +0530

    add

--- Arrays/09.linearsearch.py
+++ Arrays/09.linearsearch.py
@@ -4,8 +4,8 @@ def linear(arr,target):
            return i
     return -1
 
-arr = [3,5,2,5,7]
-target = 2
+arr = [3,5,2,50,7,6]
+target = 7
 result = linear(arr,target)
 if result != -1:
     print(result)
