30e8184dfb035f4efa4a058d12a6913a95e891b0
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-13 16:17:45 +0530

    Missing Element

--- Arrays/11.missing_element.py
+++ Arrays/11.missing_element.py
@@ -8,22 +8,11 @@ def missing(arr):
     missing_num = totalsum - arr_sum
     return missing_num
 
-arr = [1,3,4,5,6,7,8,9,10]
+arr = [1,2,3,4,5,6,8,9,10]
 mising_number = missing(arr)
 print("Missing Element :",mising_number)
 
 """ 
 OUTPUT:
-Missing Element : 2
-
-EXAPLANATION:
- n  = 5
-* The given array is [1,3,4, 5].
-* The length of the array is 4, so the expected length (if no number is missing) is 5.
-* The expected sum of all numbers from 1 to 5 is calculated as (5 * (5 + 1)) / 2 = 15.
-* The actual sum of the numbers in the array [1, 2, 3, 5] is 1 + 3 + 4 + 5 = 13.
-* The missing number is calculated as expected_sum - actual_sum = 15 - 13 = 2.
-* Therefore, the missing number in the array is 2.
-* The code prints: The missing number is: 2
-
+Missing Element : 
  """
