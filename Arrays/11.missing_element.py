1071fb3887cbbd32ba1b2c6476a8e4994d30e343
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-13 16:18:31 +0530

    M

--- Arrays/11.missing_element.py
+++ Arrays/11.missing_element.py
@@ -8,7 +8,7 @@ def missing(arr):
     missing_num = totalsum - arr_sum
     return missing_num
 
-arr = [1,2,3,4,5,6,8,9,10]
+arr = [1,2,3,4,6,7,8,9,10]
 mising_number = missing(arr)
 print("Missing Element :",mising_number)
 
