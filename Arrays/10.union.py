850789803a89cb268880b6960e232838fd040b52
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-19 11:38:44 +0530

    Union Sort

--- Arrays/10.union.py
+++ Arrays/10.union.py
@@ -1,1 +1,29 @@
     # Union of two sorted arrays.
+def union_of_sorted_arrays(arr1, arr2):
+    union_set = set()
+    i, j = 0, 0
+
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            union_set.add(arr1[i])
+            i += 1
+        elif arr1[i] > arr2[j]:
+            union_set.add(arr2[j])
+            j += 1
+        else:
+            union_set.add(arr1[i])
+            i += 1
+            j += 1
+# Add remaining elements of arr1, if any
+    while i < len(arr1):
+        union_set.add(arr1[i])
+        i += 1
+# Add remaining elements of arr2, if any
+    while j < len(arr2):
+        union_set.add(arr2[j])
+        j += 1
+
+    return sorted(union_set)
+arr1 = [1, 3, 4, 5, 7]
+arr2 = [2, 3, 5, 6]
+print(union_of_sorted_arrays(arr1, arr2))
