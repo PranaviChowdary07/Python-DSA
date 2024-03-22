9cf0bff06d68cf6a4fed6d5d75452356264c9d11
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-22 12:58:03 +0530

    Largest subArray with K

--- Arrays/14.largest_subarray_K.py
+++ Arrays/14.largest_subarray_K.py
@@ -1,5 +1,6 @@
 # Function to find the length of the largest subarray with sum K
 # Brute code
+""" 
 def maxSubArrayLen(nums, k):
     max_len = 0
     n = len(nums)
@@ -16,4 +17,28 @@ def maxSubArrayLen(nums, k):
 # Example usage
 nums = [1, 1, 1, 2, 3,3, 4]
 k = 5
-print("Length of the largest subarray with sum", k, "is:", maxSubArrayLen(nums, k))
+print("Length of the largest subarray with sum", k, "is:", maxSubArrayLen(nums, k)) """
+
+# Optimal code:
+def subArray(nums,k):
+    max_len = 0
+    sum_index ={0: -1}
+    current_sum = 0
+
+    for i ,num in enumerate(nums):
+        current_sum += num
+        difference = current_sum -k
+        if difference in sum_index:
+            max_len = max(max_len,i - sum_index[difference])
+        # Store the running sum and its corresponding index if not already in sum_to_index
+        if current_sum not in sum_index:
+            sum_index[current_sum] = i
+    return max_len
+
+nums = [1,1,2,3,4,1,1]
+k = 5
+print("Length of the largest subarray with sum",k,"is :",subArray(nums,k))
+
+
+
+        
