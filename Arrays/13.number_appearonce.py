e7fccab26976422fef23640f67046a3c5478e676
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-23 19:34:13 +0530

    Number appear at once

--- Arrays/13.number_appearonce.py
+++ Arrays/13.number_appearonce.py
@@ -7,17 +7,7 @@ def once_appear(nums):
 nums = [1,1,2,2,3,4,4,4,5,5]
 Single_number = once_appear(nums)
 print("Once apppear number is :",Single_number) """
-# Optimal solution to find the number that appears only once
-""" def once_appear(nums):
-    single = 0
-    for num in nums:
-        single ^= num
-    return single
 
-# Example usage
-nums = [1, 1, 2, 2, 3, 4, 4, 5, 5]
-single_number = once_appear(nums)
-print("Number appearing once is:", single_number) """
 # Optimal solution to find the number that appears only once
 def once_appear(nums):
     single = 0
