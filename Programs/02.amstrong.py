68c34bd046884b91a25425cc74e0e5e8aabe805e
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-26 12:05:55 +0530

    P

---
+++ Programs/02.amstrong.py
@@ -0,0 +1,26 @@
+def is_armstrong_number(number):
+    # Count number of digits
+    num_length = len(str(number))
+    
+    # Initialize sum variable
+    armstrong_sum = 0
+    temp = number
+    
+    # Calculate Armstrong sum
+    while temp > 0:
+        digit = temp % 10
+        armstrong_sum += digit ** num_length
+        temp //= 10
+    
+    # Check if the sum is equal to the original number
+    if armstrong_sum == number:
+        return True
+    else:
+        return False
+
+# Example usage:
+number = int(input("Enter a number: "))
+if is_armstrong_number(number):
+    print(number, "is an Armstrong number.")
+else:
+    print(number, "is not an Armstrong number.")
