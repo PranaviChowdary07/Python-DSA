92bc437e7eab3b61f5d12e76ccad69823655ed33
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-27 19:41:14 +0530

    year

--- Programs/03.leap_year.py
+++ Programs/03.leap_year.py
@@ -4,7 +4,20 @@ def is_leap_year(year):
     return (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0)
 year = int(input("Enter the year:"))
 if is_leap_year(year):
-    print("{year} is a leap year")
+    print(f"{year} is a leap year")
 else:
-    print("{year} is not a leap year")
+    print(f"{year} is not a leap year")
+
+# Another Way
+yr = int(input("Enter the year:"))
+if yr %4 ==0:
+    if yr % 100 ==0:
+        if yr % 400 ==0:
+            print(yr,"is a leap year")
+        else:
+            print(yr,"is not a leap year")
+    else:
+        print(yr,"is a leap year")
+else:
+    print(yr,"is not a leap year")
 
