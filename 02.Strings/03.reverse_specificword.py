7e906cbe9443f81326658f10412b4c0cb16889c1
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-29 16:31:18 +0530

    reverse specific word in line

---
+++ 02.Strings/03.reverse_specificword.py
@@ -0,0 +1,11 @@
+def reverse_word_in_sentence(sentence, target_word):
+    words = sentence.split()  
+    for i, word in enumerate(words):
+        if word == target_word:
+            words[i] = word[::-1]  # Reverse the target word
+
+    return ' '.join(words)  # Join the modified words back into a string
+sentence = "Pranavi learns Python"
+target_word = "learns"
+output_sentence = reverse_word_in_sentence(sentence, target_word)
+print("Output:", output_sentence)
