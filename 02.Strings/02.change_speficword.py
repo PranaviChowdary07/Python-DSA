7763ad8237adaab3d056c36e412770ffe272e2bf
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-29 16:49:33 +0530

    Change specific word in text

--- 02.Strings/02.change_speficword.py
+++ 02.Strings/02.change_speficword.py
@@ -10,23 +10,34 @@ def replace_word_in_line():
 
     output_str = ' '.join(words)  # Join the modified words back into a string
     print("Output:", output_str)
-replace_word_in_line()
+replace_word_in_line() 
+
+""" 
+Output:
+Enter a line of text: Pranavi learn python
+Enter the word to replace: Pranavi
+Enter the new word: Lavanya
+Output: Lavanya learn python
+ """
 
 
 # another code already text exist
-def replace_word_in_line():
-    input_str = input("Enter a line of text: ")  # Take input from the user
-    old_word = input("Enter the word to replace: ")
-    new_word = input("Enter the new word: ")
-
+def replace_word(input_str, old_word, new_word):
     words = input_str.split()  # Split the string into words
     for i, word in enumerate(words):
         if word == old_word:
             words[i] = new_word  # Replace the old word with the new word
+    return ' '.join(words)  # Join the modified words back into a string
+input_str = "pranavi learn python"
+old_word = "learn"
+new_word = "study"
+output_str = replace_word(input_str, old_word, new_word)
+print("Output:", output_str)
 
-    output_str = ' '.join(words)  # Join the modified words back into a string
-    print("Output:", output_str)
 
-# Call the function to replace the word in the line provided by the user
-replace_word_in_line()
+""" 
+Output:
+
+Output: pranavi study python
+ """
 
