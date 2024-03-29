7c580ab557fec6b453054eb464739bf24e332bab
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-29 16:22:30 +0530

    change specific word

---
+++ 02.Strings/02.change_speficword.py
@@ -0,0 +1,13 @@
+def replace_word_in_line():
+    input_str = input("Enter a line of text: ")  # Take input from the user
+    old_word = input("Enter the word to replace: ")
+    new_word = input("Enter the new word: ")
+
+    words = input_str.split()  # Split the string into words
+    for i, word in enumerate(words):
+        if word == old_word:
+            words[i] = new_word  # Replace the old word with the new word
+
+    output_str = ' '.join(words)  # Join the modified words back into a string
+    print("Output:", output_str)
+replace_word_in_line()
