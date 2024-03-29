7d8cdaa361823df640476a33340dd9488fef9874
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-03-29 16:08:22 +0530

    Reverse word not sentence

--- 02.Strings/01.reverseword_notsentence.py
+++ 02.Strings/01.reverseword_notsentence.py
@@ -1,7 +1,7 @@
 def reverse_words(input_str):
     words = input_str.split()  # Split the string into words
     reversed_words = [word[::-1] for word in words]  # Reverse each word
-    return ' '.join(reversed_words)  # Join the reversed words back into a string
+    return ' '.join(reversed_words)  # Join the reverse words. ' ' --> using for space in words
 input_str = input("Enter the sentence:")
 #input_str = "pranavi learn python"
 output_str = reverse_words(input_str)
