bcc225ecf37bbe4dc8f66d168b6bc7d6a782bd0f
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-04-02 11:20:27 +0530

    Specific word change

--- 02.Strings/02.change_speficword.py
+++ 02.Strings/02.change_speficword.py
@@ -26,9 +26,9 @@ def replace_word(input_str, old_word, new_word):
     words = input_str.split()  # Split the string into words
     for i, word in enumerate(words):
         if word == old_word:
-            words[i] = new_word  # Replace the old word with the new word
+            words[i] = new_word   # Replace the old word with the new word
     return ' '.join(words)  # Join the modified words back into a string
-input_str = "pranavi learn python"
+input_str = "pranavi learn python" 
 old_word = "learn"
 new_word = "study"
 output_str = replace_word(input_str, old_word, new_word)
