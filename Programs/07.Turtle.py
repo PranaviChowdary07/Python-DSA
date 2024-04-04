6398974ae90650af01dcd066ed8515c627ef938f
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-04-04 15:36:04 +0530

    flwr

---
+++ Programs/07.Turtle.py
@@ -0,0 +1,19 @@
+import turtle as t
+import colorsys
+
+t.speed(0)
+t.bgcolor("black")
+h = 0
+for i in range(16):
+    for j in range(18):
+        c = colorsys.hsv_to_rgb(h,1,1)
+        t.color(c)
+        h += 0.005
+        t.rt(90)
+        t.circle(150 - j * 6,90)
+        t.lt(90)
+        t.circle(150 - j* 6,90)
+        t.rt(180)
+    t.circle(40,24)
+print()
+
