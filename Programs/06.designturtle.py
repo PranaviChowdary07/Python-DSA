448f6f5018505d32f6f1bd1abcc89c74708f3c59
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-04-04 15:26:45 +0530

    Design Turtle

--- Programs/06.designturtle.py
+++ Programs/06.designturtle.py
@@ -1,8 +1,9 @@
 import turtle
 turtle.bgcolor("black")
 squary = turtle.Turtle()
-squary.speed(10)
-squary.pencolor("red")
-for i in range(400):
+squary.speed(30)
+squary.pencolor("green")
+for i in range(300):
     squary.forward(i)
     squary.left(91)
+    
