a9f8f4447d0b084b20c96bf93f433e10e0c29cbd
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-04-06 11:27:26 +0530

    Add mesage

--- Programs/08.turtle_name.py
+++ Programs/08.turtle_name.py
@@ -17,6 +17,8 @@ def heart():
     t.lt(140)
     t.fd(113)
     curve()
+    t.lt(120)
+    curve()
     t.fd(112)
     t.end_fill()
 heart()
@@ -28,3 +30,7 @@ def write(message,pos):
     t.color('white')
     style = ('Stencil Std',18,'italic')
     t.write(message,font=style)
+
+write('HPY',(-68,95))
+write('BDY',(-55,95))
+write('BR0'(-42,95))
