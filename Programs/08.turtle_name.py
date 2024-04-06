1023eb0111153b552a053ee5343c2e858a6a3fe5
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-04-06 11:17:13 +0530

    Add def

--- Programs/08.turtle_name.py
+++ Programs/08.turtle_name.py
@@ -1,1 +1,18 @@
 import turtle
+from turtle import *
+
+wn = Screen()
+wn.bgcolor('green')
+t = turtle.Turtle()
+t.pencolor('white')
+
+def curve():
+    for i in range(200):
+        t.rt(1)
+        t.fd(1)
+
+def heart():
+    t.fillcolor('red')
+    t.begin_fill()
+    t.lt(140)
+    
